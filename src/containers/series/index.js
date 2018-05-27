32b8d072f88f26ba2e89c7bf2970ef5c0b458387
jeevan-jp <jiwanprakash65@gmail.com>
2018-05-28 01:42:55 +0530

    Added series list

--- src/containers/series/index.js
+++ src/containers/series/index.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
+import SeriesList from '../seriesList';
 
 class Series extends Component {
+  state = {
+      series: []
+  }
 
-    state = {
-        series: []
-    }
+  componentDidMount() {
+  fetch('https://api.tvmaze.com/search/shows?q=Vikings')
+    .then(res => res.json())
+    .then(json => this.setState({ series: json}));
+  }
 
-    componentDidMount() {
-    fetch('https://api.tvmaze.com/search/shows?q=Vikings')
-        .then(res => res.json())
-        .then(json => this.setState({ series: json}));
-    }
-
-    render() {
-        return (
-            <div>
-                Length of series array: { this.state.series.length }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+          Length of series array: { this.state.series.length }
+          <SeriesList list={this.state.series} />
+      </div>
+    )
+  }
 }
 
 export default Series;
