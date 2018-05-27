0f6f83334095883252a05428464996d709f856a5
jeevan-jp <jiwanprakash65@gmail.com>
2018-05-28 02:05:19 +0530

    React forms

--- src/containers/series/index.js
+++ src/containers/series/index.js
@@ -6,15 +6,19 @@ class Series extends Component {
       series: []
   }
 
-  componentDidMount() {
-  fetch('https://api.tvmaze.com/search/shows?q=Vikings')
-    .then(res => res.json())
-    .then(json => this.setState({ series: json}));
+  onSeriesInputChange = e => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${e.target.value}`)
+      .then(res => res.json())
+      .then(json => this.setState({ series: json}));
   }
 
   render() {
     return (
       <div>
+          <p>
+            Enter name to search: <br />
+            <input type="text" onChange={this.onSeriesInputChange} />
+          </p>
           Length of series array: { this.state.series.length }
           <SeriesList list={this.state.series} />
       </div>
